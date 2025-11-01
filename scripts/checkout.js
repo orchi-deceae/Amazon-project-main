069c9bf6f823e4904d4bc050cdafb7ca4d465825
oksherlock <oksherlock9661@gmail.com>
2025-11-01 17:38:19 +0100

    0.42.1 - "checkout.js"

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,6 +3,18 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import '../data/car.js'
 
-renderOrderSummary()
-renderPaymentSummary()
-renderCheckoutHeader()
+async function loadPage(){
+    try {
+        await loadProductsFetch()
+        await new Promise((resolve, reject)=>{
+            loadCart(()=>{resolve()});
+        })
+        renderOrderSummary()
+        renderPaymentSummary()
+        renderCheckoutHeader()
+    }
+    catch(err) {
+        console.log('Unexpected error, Please try again later.\n' + err)
+    }
+}
+loadPage()
